b510802eed6751d2eb2a7c97122916b8cf096af3
yakoumobi-sys <yakoumobi@gmail.com>
2026-06-19 13:51:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@imgly/background-removal'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), '@imgly/background-removal']
-    }
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules\/@imgly/,
-      type: 'javascript/auto',
-    })
-    return config
-  },
 }
 
 module.exports = nextConfig
