f5a7725f53c7ea806a980cd25afe81e33201b0fe
yakoumobi-sys <yakoumobi@gmail.com>
2026-06-28 00:44:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className={inter.variable}>
-      <body className="font-sans">{children}</body>
+      <body className="font-sans bg-[#0C4A6E]">{children}</body>
     </html>
   )
 }
