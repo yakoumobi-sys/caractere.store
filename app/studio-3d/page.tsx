2c772ef6a65d1fe915c3c1c10560e8beb8d9d131
yakoumobi-sys <yakoumobi@gmail.com>
2026-07-06 15:57:01 +0100

    Update page.tsx

--- app/studio-3d/page.tsx
+++ app/studio-3d/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useState, useRef, Suspense } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import Navbar from '@/components/layout/Navbar'
